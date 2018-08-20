9a54171f5c554616aaf9facfc25b2c368a6e29e3
Christopher Chute <chutechristopher@gmail.com>
2018-08-20 11:22:30 -0700

    Add config template.

--- scripts/client.py
+++ scripts/client.py
@@ -5,7 +5,7 @@ from client import PBTClient
 
 
 def main(args):
-    client = PBTClient(args.client_id, args.server_ip, args.server_port, args.auth_key)
+    client = PBTClient(args.client_id, args.server_url, args.server_port, args.auth_key)
 
     for _ in range(args.num_epochs):
         client.train_epoch()
@@ -20,12 +20,14 @@ if __name__ == '__main__':
     # PBT Connection Settings
     parser.add_argument('--client_id', type=int, required=True,
                         help='ID of PBT client. Must be unique for each member of the population.')
-    parser.add_argument('--server_ip', type=str, required=True,
-                        help='IP address of PBT server.')
+    parser.add_argument('--server_url', type=str, required=True,
+                        help='URL or IP address of PBT server.')
     parser.add_argument('--server_port', type=int, default=7777,
                         help='Port on which the server listens for clients.')
     parser.add_argument('--auth_key', type=str, default='insecure',
                         help='Key for clients to authenticate with server.')
+    parser.add_argument('--config_path', type=str, required=True,
+                        help='Path to configuration file with hyperparameters (see templates).')
 
     # Training Settings
     parser.add_argument('--num_epochs', type=int, default=10,
