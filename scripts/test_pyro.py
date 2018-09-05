c4393463fe0653a1d30ad22c07143b524b41023f
Christopher Chute <chutechristopher@gmail.com>
2018-09-04 18:13:08 -0700

    Add name server to warehouse.

--- scripts/test_pyro.py
+++ scripts/test_pyro.py
@@ -1,16 +1,29 @@
 import argparse
-import Pyro4 as pyro
+import Pyro4
+import Pyro4.util
+import sys
 
 
 def main(args):
-    warehouse = Warehouse()
-    janet = Person('Janet')
-    henry = Person('Henry')
 
-    janet.visit(warehouse)
-    henry.visit(warehouse)
+    if args.warehouse:
+        # Start Pyro4 daemon
+        Pyro4.Daemon.serveSimple({Warehouse: "example.warehouse"}, ns=True)
+    else:
+        sys.excepthook = Pyro4.util.excepthook
+
+        # Visit the warehouse
+        warehouse = Pyro4.Proxy('PYRONAME:example.warehouse')
+
+        janet = Person('Janet')
+        henry = Person('Henry')
+
+        janet.visit(warehouse)
+        henry.visit(warehouse)
 
 
+@Pyro4.expose
+@Pyro4.behavior(instance_mode="single")
 class Warehouse(object):
     """Warehouse class from official Pyro4 tutorial."""
     def __init__(self):
@@ -45,7 +58,7 @@ class Person(object):
             warehouse.store(self.name, item)
 
     def retrieve(self, warehouse):
-        print(f'The warehouse contains: {warehouse.contents}')
+        print(f'The warehouse contains: {warehouse.list_contents()}')
         item = input('Type something you want to take (or empty): ').strip()
         if item:
             warehouse.take(self.name, item)
@@ -53,5 +66,7 @@ class Person(object):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+
+    parser.add_argument('--warehouse', action='store_true')
 
     main(parser.parse_args())
